a55de355155290038e2e91d4b3ccddab786b5e15
ShiniYanada <ysthon@gmail.com>
2018-11-13 17:14:20 +0900

    Add Post model and user-post relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
 
+  resources :users
+  get '/signup', to: 'users#new'
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
