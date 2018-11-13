a4d1f12395598a0b264264941125909c2b1d732a
ShiniYanada <ysthon@gmail.com>
2018-11-13 16:40:50 +0900

    Add Users Controller and Model

---
+++ db/migrate/20181113073140_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :identifier
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+
+    add_index :users, :identifier, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, [:email, :unique], unique: true
+  end
+end
