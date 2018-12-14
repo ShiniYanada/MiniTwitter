ab51f66b6ab7197b06e20b4b9267b24419310bfd
ShiniYanada <ysthon@gmail.com>
2018-12-14 10:55:37 +0900

    Add follow-function by ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,15 @@
 class RelationshipsController < ApplicationController
 
   def create
-    user = User.find_by(id: params[:following_id])
-    current_user.follow(user)
-    redirect_back(fallback_location: root_url)
+    @path = Rails.application.routes.recognize_path(request.referer)
+    @user = User.find_by(id: params[:following_id])
+    current_user.follow(@user)
+
   end
 
   def destroy
-    user = Relationship.find(params[:id]).following
-    current_user.unfollow(user)
-    redirect_back(fallback_location: root_url)
+    @path = Rails.application.routes.recognize_path(request.referer)
+    @user = Relationship.find(params[:id]).following
+    current_user.unfollow(@user)
   end
 end
